8187da07bb9c429848d5e12161aa280d95d6ac97
Ramsey <111786608+Keilaramos19@users.noreply.github.com>
2023-03-27 11:37:01 -0400

    Update data.sql

--- projeto_auto_escola/src/main/resources/data.sql
+++ projeto_auto_escola/src/main/resources/data.sql
@@ -1,6 +1,40 @@
 
-INSERT INTO aluno (nome, cpf, telefone,logradouro, numero, complemento, bairro, cep, cidade, estado) values 
-('Maria Rita','01001789288', '9299545485', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '69.401-12', 'Itacoatiara', 'AM');
+INSERT INTO usuario (nome, email, senha) values ('Administrador','admin@uea.edu.br','{bcrypt}$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (nome, email, senha) values ('Maria Silva','maria@uea.edu.br','{bcrypt}$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_INSTRUTOR');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_INSTRUTOR');
+INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_INSTRUTOR');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_INSTRUTOR');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_ALUNO');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_ALUNO');
+INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_ALUNO');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_ALUNO');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_AULA');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_AULA');
+INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_AULA');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_AULA');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_USUARIO');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_USUARIO');
+INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_USUARIO');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_USUARIO');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_PERMISSAO');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_PERMISSAO');
+INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_PERMISSAO');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_PERMISSAO');
+
+INSERT INTO usuario_permissao (codigo_usuario,codigo_permissao) values (1, 1), (1, 2),
+(1, 3), (1, 4),(1, 5), (1, 6),(1, 7), (1, 8),(1, 9), (1, 10),(1, 11), (1, 12), (1, 13),(1, 14), (1, 15),(1, 16), (1, 17), (1, 18), (1, 19), (1, 20);
+
+INSERT INTO usuario_permissao (codigo_usuario,codigo_permissao) values (2, 2), (2, 6), (2, 10);
+
+
+
+INSERT INTO aluno (nome, cpf, telefone,logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Maria Rita','01001789288', '9299545485', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '69.401-12', 'Itacoatiara', 'AM');
 INSERT INTO aluno (nome, cpf, telefone,logradouro, numero, complemento, bairro, cep, cidade, estado) values 
 ('Rita Cassia','023568987', '548569875', 'Rua do Santos', '10', null, 'Salvado', '69.400-12', 'Itacotiara', 'AM');
 INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
@@ -51,97 +85,89 @@ INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro,
 ('Diego', '33344455566', '11854321098', 'Rua M', '2829', '', 'Tatuapé', '13013000', 'São Paulo', 'SP');
 INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
 ('Elisa', '44455566677', '11843210987', 'Rua N', '3031', '', 'Jabaquara', '14014000', 'São Paulo', 'SP');
- INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Ana Silva', '12345678901', '11999990001', 'Rua A', '100', '', 'Centro', '01001000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Bruno Santos', '23456789012', '11999990002', 'Rua B', '200', 'Apto 12', 'Jardim', '02002000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Camila Souza', '34567890123', '11999990003', 'Rua C', '300', '', 'Vila', '03003000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Daniel Lima', '45678901234', '11999990004', 'Rua D', '400', 'Sala 3', 'Perdizes', '04004000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Eduarda Oliveira', '56789012345', '11999990005', 'Rua E', '500', '', 'Moema', '05005000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Fernando Martins', '67890123456', '11999990006', 'Rua F', '600', '', 'Morumbi', '06006000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Gabriela Costa', '78901234567', '11999990007', 'Rua G', '700', '', 'Lapa', '07007000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Henrique Pereira', '89012345678', '11999990008', 'Rua H', '800', '', 'Butantã', '08008000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Isabela Almeida', '90123456789', '11999990009', 'Rua I', '900', '', 'Pinheiros', '09009000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('João Santos', '01234567890', '11999990010', 'Rua J', '1000', 'Casa 2', 'Itaim Bibi', '10010000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Karina Vieira', '12345678901', '21999990001', 'Rua K', '1100', '', 'Botafogo', '20020000', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Lucas Silva', '23456789012', '21999990002', 'Rua L', '1200', '', 'Tijuca', '21021000', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Mariana Costa', '34567890123', '21999990003', 'Rua M', '1300', '', 'Ipanema', '22022000', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Nathalia Santos', '45678901234', '21999990004', 'Rua N', '1400', 'Apto 5', 'Copacabana', '23023000', 'Rio de Janeiro', 'RJ');
-   INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('João Silva', '12345678901', '1199999999', 'Rua A', '100', '', 'Centro', '00000000', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Maria Santos', '23456789012', '2199999999', 'Avenida B', '200', 'Sala 10', 'Copacabana', '11111111', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Pedro Souza', '34567890123', '3199999999', 'Rua C', '300', '', 'Vila Mariana', '22222222', 'Belo Horizonte', 'MG');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Ana Oliveira', '45678901234', '4199999999', 'Rua D', '400', '', 'Liberdade', '33333333', 'Curitiba', 'PR');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Luiz Ferreira', '56789012345', '2199999999', 'Rua E', '500', 'Apto 20', 'Barra da Tijuca', '44444444', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Carla Rodrigues', '67890123456', '5199999999', 'Avenida F', '600', '', 'Santo Antônio', '55555555', 'Porto Alegre', 'RS');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Fernando Nunes', '78901234567', '8199999999', 'Rua G', '700', '', 'Ipanema', '66666666', 'Recife', 'PE');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Sofia Costa', '89012345678', '2199999999', 'Rua H'
-  , '800', '', 'Jardins', '77777777', 'São Paulo', 'SP');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Mariana Mendes', '90123456789', '2199999999', 'Rua I', '900', '', 'Botafogo', '88888888', 'Rio de Janeiro', 'RJ');
-  INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Lucas Santos', '01234567890', '2199999999', 'Avenida J', '1000', '', 'Gávea', '99999999', 'Rio de Janeiro', 'RJ');
- INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
-  ('Suzyane Brito', '01234567890', '2199999999', 'Avenida J', '1000', '', 'Gávea', '99999999', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Ana Silva', '12345678901', '11999990001', 'Rua A', '100', '', 'Centro', '01001000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Bruno Santos', '23456789012', '11999990002', 'Rua B', '200', 'Apto 12', 'Jardim', '02002000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Camila Souza', '34567890123', '11999990003', 'Rua C', '300', '', 'Vila', '03003000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Daniel Lima', '45678901234', '11999990004', 'Rua D', '400', 'Sala 3', 'Perdizes', '04004000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Eduarda Oliveira', '56789012345', '11999990005', 'Rua E', '500', '', 'Moema', '05005000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Fernando Martins', '67890123456', '11999990006', 'Rua F', '600', '', 'Morumbi', '06006000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Gabriela Costa', '78901234567', '11999990007', 'Rua G', '700', '', 'Lapa', '07007000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Henrique Pereira', '89012345678', '11999990008', 'Rua H', '800', '', 'Butantã', '08008000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Isabela Almeida', '90123456789', '11999990009', 'Rua I', '900', '', 'Pinheiros', '09009000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('João Santos', '01234567890', '11999990010', 'Rua J', '1000', 'Casa 2', 'Itaim Bibi', '10010000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Karina Vieira', '12345678901', '21999990001', 'Rua K', '1100', '', 'Botafogo', '20020000', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Lucas Silva', '23456789012', '21999990002', 'Rua L', '1200', '', 'Tijuca', '21021000', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Mariana Costa', '34567890123', '21999990003', 'Rua M', '1300', '', 'Ipanema', '22022000', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Nathalia Santos', '45678901234', '21999990004', 'Rua N', '1400', 'Apto 5', 'Copacabana', '23023000', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('João Silva', '12345678901', '1199999999', 'Rua A', '100', '', 'Centro', '00000000', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Maria Santos', '23456789012', '2199999999', 'Avenida B', '200', 'Sala 10', 'Copacabana', '11111111', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Pedro Souza', '34567890123', '3199999999', 'Rua C', '300', '', 'Vila Mariana', '22222222', 'Belo Horizonte', 'MG');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Ana Oliveira', '45678901234', '4199999999', 'Rua D', '400', '', 'Liberdade', '33333333', 'Curitiba', 'PR');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Luiz Ferreira', '56789012345', '2199999999', 'Rua E', '500', 'Apto 20', 'Barra da Tijuca', '44444444', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Carla Rodrigues', '67890123456', '5199999999', 'Avenida F', '600', '', 'Santo Antônio', '55555555', 'Porto Alegre', 'RS');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Fernando Nunes', '78901234567', '8199999999', 'Rua G', '700', '', 'Ipanema', '66666666', 'Recife', 'PE');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Sofia Costa', '89012345678', '2199999999', 'Rua H', '800', '', 'Jardins', '77777777', 'São Paulo', 'SP');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Mariana Mendes', '90123456789', '2199999999', 'Rua I', '900', '', 'Botafogo', '88888888', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Lucas Santos', '01234567890', '2199999999', 'Avenida J', '1000', '', 'Gávea', '99999999', 'Rio de Janeiro', 'RJ');
+INSERT INTO Aluno (nome, cpf, telefone, logradouro, numero, complemento, bairro, cep, cidade, estado)values
+('Suzyane Brito', '01234567890', '2199999999', 'Avenida J', '1000', '', 'Gávea', '99999999', 'Rio de Janeiro', 'RJ');
 
-INSERT INTO Instrutor (nome) values('Fulano de Tal');
-INSERT INTO Instrutor (nome) values('Beltrano da Silva');
-INSERT INTO Instrutor (nome) values('Ciclana Oliveira');
-INSERT INTO Instrutor (nome) values('João dos Santos');
+
+INSERT INTO Instrutor (nome) values('Joao Dos Santos');
+INSERT INTO Instrutor (nome) values('Bel da Silva');
+INSERT INTO Instrutor (nome) values('Carla Oliveira');
+INSERT INTO Instrutor (nome) values('Estrela dos Santos');
 INSERT INTO Instrutor (nome) values('Maria Fernandes');
 
+INSERT INTO Instrutor (nome) values('Marcos Rios');
+INSERT INTO Instrutor (nome) values('Juliana Fernandes');
+INSERT INTO Instrutor (nome) values('Antonio barbosa');
+INSERT INTO Instrutor (nome) values('Matheus Henrique');
+INSERT INTO Instrutor (nome) values('Andre Martins');
 
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula teórica', '2022-01-01', 100.00, 'Manhã', 1, 1);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula prática', '2022-01-02', 150.00, 'Tarde', 2, 1);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula simulada', '2022-01-03', 120.00, 'Manhã', 1, 2);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula de direção defensiva', '2022-01-04', 80.00, 'Tarde', 2, 2);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula de primeiros socorros', '2022-01-05', 70.00, 'Manhã', 3, 3);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula teórica', '2022-01-01', 100.00, 'Manhã', 1, 1);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula prática', '2022-01-02', 150.00, 'Tarde', 2, 1);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula simulada', '2022-01-03', 120.00, 'Manhã', 10, 5);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula de direção defensiva', '2022-01-04', 80.00, 'Tarde', 8, 5);
+INSERT INTO Aula (descricao, data_inicio, valor, turno, codigo_aluno, codigo_instrutor)values
+('Aula de primeiros socorros', '2022-01-05', 70.00, 'Manhã', 1, 3);
 
-
-INSERT INTO usuario (nome, email, senha) values ('Administrador','admin@uea.edu.br','{bcrypt}$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
-INSERT INTO usuario (nome, email, senha) values ('Maria Silva','maria@uea.edu.br','{bcrypt}$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
-
-INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_INSTRUTOR');
-INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_INSTRUTOR');
-INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_INSTRUTOR');
-INSERT INTO permissao (descricao) values ('ROLE_REMOVER_INSTRUTOR');
-
-INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_ALUNO');
-INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_ALUNO');
-INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_ALUNO');
-INSERT INTO permissao (descricao) values ('ROLE_REMOVER_ALUNO');
-
-INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_AULA');
-INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_AULA');
-INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_AULA');
-INSERT INTO permissao (descricao) values ('ROLE_REMOVER_AULA');
-
-INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_USUARIO');
-INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_USUARIO');
-INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_USUARIO');
-INSERT INTO permissao (descricao) values ('ROLE_REMOVER_USUARIO');
-
-INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_PERMISSAO');
-INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_PERMISSAO');
-INSERT INTO permissao (descricao) values ('ROLE_ATUALIZAR_PERMISSAO');
-INSERT INTO permissao (descricao) values ('ROLE_REMOVER_PERMISSAO');
-
-INSERT INTO usuario_permissao (codigo_usuario,codigo_permissao) values (1, 1), (1, 2),
-(1, 3), (1, 4),(1, 5), (1, 6),(1, 7), (1, 8),(1, 9), (1, 10),(1, 11), (1, 12), (1, 13),(1, 14), (1, 15),(1, 16), (1, 17), (1, 18), (1, 19), (1, 20);
-
-INSERT INTO usuario_permissao (codigo_usuario,codigo_permissao) values (2, 2), (2, 6), (2, 10);
 
